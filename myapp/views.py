f6ad227eaa18a48871b310ebe397d6c29004231c
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-12-03 21:09:21 +0200

    Created expense model and form, created view with the form and a table showing added expenses.

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .forms import ExpenseForm
+from .models import Expense
 # Create your views here.
+
+
+def index(request):
+    expenses = Expense.objects.all()
+    if request.method == 'POST':
+        expense_form = ExpenseForm(request.POST)
+        if expense_form.is_valid():
+            expense_form.save()
+        return render(request, 'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses})
+    else:
+        expense_form = ExpenseForm()
+        return render(request, 'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses})
+
+    
+      
